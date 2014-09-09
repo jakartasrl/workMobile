dfed0e803432feec481fe560ac4b1bea2915dc5e
Leo <ss.leo.10@gmail.com>
2014-09-09 16:58:38 -0300

    Agregadas operaciones vacias de traer clasificadores

---
+++ Common/Framework/java/clientes/src/main/java/com/jkt/erp/operaciones/TraerClasifCliente.java
@@ -0,0 +1,14 @@
+package com.jkt.erp.operaciones;
+
+import java.util.Map;
+
+import com.jkt.operaciones.Operation;
+
+public class TraerClasifCliente extends Operation {
+
+	@Override
+	public void execute(Map<String, Object> aParams) throws Exception {
+
+	}
+
+}
