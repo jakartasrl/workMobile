a7e385426f40952eb66fd776faadfbdd2bda7cb8
Leo Suarez <ss.leo.10@gmail.com>
2014-10-29 14:51:58 -0300

    Modificando campos de traer tipos de ventas.

--- JakartaERP/java/ventas/src/main/java/com/jkt/cotizador/operaciones/TraerTiposDeVenta.java
+++ JakartaERP/java/ventas/src/main/java/com/jkt/cotizador/operaciones/TraerTiposDeVenta.java
@@ -1,20 +1,24 @@
 package com.jkt.cotizador.operaciones;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 import com.jkt.dominio.Container;
 import com.jkt.operaciones.Operation;
 
+/**
+ * <p>Esta operación retorna una lista de posibles tipos de venta.</p>
+ * <p>Este tipo de venta estará ligado a un detalle de cotización o pedido.</p>
+ * 
+ * @author Leonel Suarez - Jakarta SRL
+ */
 public class TraerTiposDeVenta extends Operation {
+
+	private static final String WRITER_ENTIDADES = "entidades";
 
 	@Override
 	public void execute(Map<String, Object> aParams) throws Exception {
-		List<Container> entidades = Arrays.asList(new Container("1", "Venta"),new Container("2", "Reparacion"));
-		for (Container container : entidades) {
-			notificarObjeto("entidades", container);
-		}
+		notificarObjetos(WRITER_ENTIDADES, Arrays.asList(new Container("1", "Fabricacion"), new Container("2", "Reparacion")));
 	}
 
 }
