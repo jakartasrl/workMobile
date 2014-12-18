a9fe72cf0bf908a0497000c871ba4ae1bbeb2a11
Leo Suarez <ss.leo.10@gmail.com>
2014-12-18 15:04:05 -0300

    Cambios para la tabla de parametros

--- JakartaERP/java/ventas/src/main/java/com/jkt/dominio/ComprobanteVentaDet.java
+++ JakartaERP/java/ventas/src/main/java/com/jkt/dominio/ComprobanteVentaDet.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.jkt.cotizador.dominio.Cotizador;
+import com.jkt.dominio.TipoComprobante.Comportamiento;
 import com.jkt.erp.articulos.Producto;
+import com.jkt.excepcion.JakartaException;
 import com.jkt.varios.dominio.Moneda;
 import com.jkt.varios.dominio.UnidadMedida;
 
@@ -26,8 +28,27 @@ public class ComprobanteVentaDet extends PersistentEntity {
 	private Cotizador cotizador;
 	private int tipoVenta=0;
 	private String referencia;
+	private int estadoId;
 	
-	
+	public int getEstadoId() {
+		return estadoId;
+	}
+
+	public void setEstadoId(int estadoId) {
+		this.estadoId = estadoId;
+	}
+
+	/**
+	 * Este metodo retorna la descripcion del estado solicitado.
+	 * 
+	 * throw {@link JakartaException}
+	 * 
+	 */
+	public String getDescripcionEstado() throws JakartaException{
+		Estado estado = Estado.getEstado(this.estadoId);
+		return estado.descripcion();
+	}
+
 	/*
 	 * Helper methods
 	 */
@@ -143,5 +164,74 @@ public class ComprobanteVentaDet extends PersistentEntity {
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+	
+	
+	public enum Estado { 
+		
+		PENDIENTE_A_COTIZAR(1) {
+			@Override
+			public String descripcion() {
+				return "Pendiente de Cotizar";
+			}
+		},
+		COTIZADO_NO_AUTORIZADO(2) {
+			@Override
+			public String descripcion() {
+				return "Cotizado pero No Autorizado";
+			}
+
+		},
+		AUTORIZADO(3) {
+			@Override
+			public String descripcion() {
+				return "Autorizado";
+			}
+
+		},
+		RECHAZADO(4) {
+			@Override
+			public String descripcion() {
+				return "Rechazado";
+			}
+
+		};
+		
+		private int id;
+		public abstract String descripcion();
+
+		private Estado (int id){
+			this.id=id;
+		}
+		
+		public int getId(){
+			return this.id;
+		}
+		
+		/**
+		 * Obtiene un elemento de la enumeracion recibiendo un entero.
+		 * 
+		 */
+		public static Estado getEstado(int value) throws JakartaException{
+			Estado[] values = values();
+			Estado e = null;
+			for (Estado estado : values) {
+				if(estado.getId()==value){
+					e=estado;
+					break; 
+				}
+			}
+			if (e==null) {
+				throw new JakartaException("No existe un Estado para la solicitud recibida.");
+			}
+			return e;
+		}
+        
+	};
+	
+	
+	public ComprobanteVentaDet() {
+		this.estadoId=Estado.PENDIENTE_A_COTIZAR.getId();
+	}
+	
 	
 }
