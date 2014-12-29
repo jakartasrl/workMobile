ff5092889b0dbc63507a753199bf5381170237fa
Leo Suarez <ss.leo.10@gmail.com>
2014-12-29 17:38:12 -0300

    Operacion traer lista de precios finalizada

---
+++ JakartaERP/java/ventas/src/main/java/com/jkt/dominio/ListaPrecios.java
@@ -0,0 +1,66 @@
+package com.jkt.dominio;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.jkt.varios.dominio.Moneda;
+
+/**
+ * <p>Una lista de precio es una lista propiamente dicha que contiene detalles las cuales brindan informacion sobre productos y sus costos.</p>
+ * 
+ * @author Leonel Suarez - Jakarta SRL
+ */
+public class ListaPrecios extends Descriptible {
+
+	private Date fechaVigenciaDesde;
+	private Date fechaVigenciaHasta;
+	private Moneda moneda;
+	private List<ListaPrecioDetalle> detalles=new ArrayList<ListaPrecioDetalle>();
+	
+	public void agregarDetalle(ListaPrecioDetalle detalle){
+		if (this.detalles.contains(detalle)) {
+			this.detalles.add(detalle);
+			detalle.setListaPrecios(this);
+		}
+	}
+	
+	
+	
+	public List<ListaPrecioDetalle> getDetalles() {
+		return detalles;
+	}
+
+
+
+	public void setDetalles(List<ListaPrecioDetalle> detalles) {
+		this.detalles = detalles;
+	}
+
+
+
+	public Date getFechaVigenciaDesde() {
+		return fechaVigenciaDesde;
+	}
+
+	public void setFechaVigenciaDesde(Date fechaVigenciaDesde) {
+		this.fechaVigenciaDesde = fechaVigenciaDesde;
+	}
+
+	public Date getFechaVigenciaHasta() {
+		return fechaVigenciaHasta;
+	}
+
+	public void setFechaVigenciaHasta(Date fechaVigenciaHasta) {
+		this.fechaVigenciaHasta = fechaVigenciaHasta;
+	}
+
+	public Moneda getMoneda() {
+		return moneda;
+	}
+
+	public void setMoneda(Moneda moneda) {
+		this.moneda = moneda;
+	}
+
+}
