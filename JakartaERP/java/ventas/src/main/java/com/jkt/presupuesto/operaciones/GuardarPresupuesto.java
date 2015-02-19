770ee9caa3217f1f32b70cbf725f59807322b967
Leo Suarez <ss.leo.10@gmail.com>
2015-02-19 18:09:28 -0300

    Agregada respuesta en el guardar presupuesto

---
+++ JakartaERP/java/ventas/src/main/java/com/jkt/presupuesto/operaciones/GuardarPresupuesto.java
@@ -0,0 +1,30 @@
+package com.jkt.presupuesto.operaciones;
+
+import java.util.List;
+import java.util.Map;
+
+import com.jkt.dominio.PersistentEntity;
+import com.jkt.excepcion.JakartaException;
+import com.jkt.excepcion.ValidacionDeNegocioException;
+import com.jkt.operaciones.Operation;
+
+public class GuardarPresupuesto extends Operation {
+
+	private static final String WRITER_PRESUPUESTO = "presupuesto";
+
+	@Override
+	public void execute(Map<String, Object> aParams) throws Exception {
+		List object = recuperarObjeto(aParams);
+		if (object == null) {
+			throw new JakartaException("Error en el archivo operaciones.xml. Dentro del tag INPUT, el atributo 'keyName' debe llamarse 'objeto'.");
+		}
+		guardar(object);
+	}
+
+	protected void guardar(List object) throws ClassNotFoundException, InstantiationException, IllegalAccessException, ValidacionDeNegocioException, JakartaException {
+		Object objectToSave = object.get(0);
+		serviceRepository.save((PersistentEntity) objectToSave);
+		notificarObjeto(WRITER_PRESUPUESTO, objectToSave);
+	}
+
+}
