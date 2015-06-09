9cb21fc7d16dea123c4b6289aa5c59ea025dca19
Leo Suarez <ss.leo.10@gmail.com>
2015-06-09 17:20:13 -0300

    Cambio en operacion TraerModeloCotizador

--- JakartaERP/java/ventas/src/main/java/com/jkt/cotizador/operaciones/TraerModeloParaCotizar.java
+++ JakartaERP/java/ventas/src/main/java/com/jkt/cotizador/operaciones/TraerModeloParaCotizar.java
@@ -97,7 +97,6 @@ public class TraerModeloParaCotizar extends AbstractRecuperarModelo {
 					
 					TituloModeloCotizador tituloModeloCotizador2 = new TituloModeloCotizador();
 
-					
 //					tituloModeloCotizador2.setId(tituloModeloCotizador.getId());
 					
 					tituloModeloCotizador2.setIdTransiente(tituloModeloCotizador.getId());
